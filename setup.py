3d6901dd9d78c9c6fd4c3eca4d1d1503aac312f3
Nicolas Mattiocco <nicolas@greenlock.fr>
2020-06-17 18:17:45 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,13 +7,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='Patrowl4py',
-<<<<<<< HEAD
-    version='1.0.6',
-    description='Python API client for Patrowl',
-=======
     version='1.1.0',
     description='Python API client for PatrowlManager (Community and Pro editions)',
->>>>>>> develop
     url='https://github.com/Patrowl/Patrowl4py',
     author='Nicolas Mattiocco',
     author_email='getsupport@patrowl.io',
